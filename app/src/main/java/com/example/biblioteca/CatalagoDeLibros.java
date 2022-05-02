f8ff4891cf4d9be3b3dfd9bece6b5d1679167baf
calde <joscalde13@gmail.com>
2022-05-02 11:40:13 -0500

    LOGIN Y REGISTRO

---
+++ app/src/main/java/com/example/biblioteca/CatalagoDeLibros.java
@@ -0,0 +1,14 @@
+package com.example.biblioteca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatalagoDeLibros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalago_de_libros);
+    }
+}
