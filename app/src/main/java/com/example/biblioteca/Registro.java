0694ec8fd6a07a7dfc9312e6361ca04bf4da60af
Danika <danikadonis@gmail.com>
2022-05-03 18:16:34 -0600

    Prueba

--- app/src/main/java/com/example/biblioteca/Registro.java
+++ app/src/main/java/com/example/biblioteca/Registro.java
@@ -1,9 +1,13 @@
 package com.example.biblioteca;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
@@ -21,7 +25,8 @@ public class Registro extends AppCompatActivity {
     EditText txtDpi;
     EditText txtCorreo;
     Button btnguardarRegistro;
-
+    private static final int STORAGE_PERMISSION_CODE = 101;
+    boolean permiso = false;
 
 
 
@@ -42,9 +47,9 @@ public class Registro extends AppCompatActivity {
 
 
 
-/**
- * Boton de registro de usuario
- */
+        /**
+        * Boton de registro de usuario
+        */
         btnguardarRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,31 +70,37 @@ public class Registro extends AppCompatActivity {
                             startActivity(navegacion);
                             Toast.makeText(Registro.this, "SE HA REGISTRADO CON EXITO", Toast.LENGTH_LONG).show();
                             }
-                            }
-                        }
-                        else{
+                        } else{
                             Toast.makeText(Registro.this, "Error al crear el usuario", Toast.LENGTH_SHORT).show();
                         }
-
                     }
-                    }
-    }
-
-
 
         });
 
 
+    }
 
+    /**
+     * Metodo que verifica si se cuenta con permiso para acceder al almacenamiento
+     * @param permission
+     * @param requestCode
+     */
+    public void checkPermission(String permission, int requestCode){
+        if (ContextCompat.checkSelfPermission(Registro.this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(Registro.this, new String[] { permission }, requestCode);
+        } else {
+            permiso = true;
+        }
+    }
 
-
-
-
-
-
-
-
-
-
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                permiso = true;
+            } else {
+                permiso = false;
+            }
+        }
     }
 }
